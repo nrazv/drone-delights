09ee7cada6c01b74d021855094f64630d142030e
Razvan-Petru Nechifor <144475822+nrazv@users.noreply.github.com>
2025-06-13 10:42:29 +0200

    Add filter components

--- frontend/src/components/SearchAndFilter.js
+++ frontend/src/components/SearchAndFilter.js
@@ -5,8 +5,30 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import InputBase from "@mui/material/InputBase";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import Menu from "@mui/material/Menu";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import MenuItem from "@mui/material/MenuItem";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const categories = Object.freeze({
+  BURGERS: "Burgers",
+  MAINS: "Mains",
+  DESSERT: "Dessert",
+  DRINKS: "Drinks",
+  SIDES: "Sides",
+});
 
 function SearchAndFilter({ handelSearch }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box mb={4} mt={4} display={"flex"} justifyContent={"center"}>
       <Paper
@@ -30,9 +52,55 @@ function SearchAndFilter({ handelSearch }) {
           <SearchIcon />
         </IconButton>
       </Paper>
-      <IconButton size="large" type="button" sx={{ marginLeft: "40px" }}>
+      <IconButton
+        size="large"
+        type="button"
+        id="filter-button"
+        sx={{ marginLeft: "40px" }}
+        onClick={handleClick}
+      >
         <FilterListIcon fontSize="large" />
       </IconButton>
+      <Menu
+        id="filter-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        variant="menu"
+      >
+        <MenuItem
+          disableRipple
+          sx={{
+            "&:hover": {
+              backgroundColor: "transparent",
+              cursor: "default",
+            },
+          }}
+        >
+          <Typography variant="subtitle1" fontWeight="bold">
+            Pick Your Craving
+          </Typography>
+        </MenuItem>
+        {Object.entries(categories).map(([key, value]) => (
+          <MenuItem disableRipple key={key} divider>
+            <FormControlLabel control={<Checkbox />} label={value} />
+          </MenuItem>
+        ))}
+
+        <Button
+          sx={{
+            width: "200px",
+            margin: "15px 5px 0px 5px",
+            textTransform: "none",
+          }}
+          variant="contained"
+          disableElevation
+        >
+          <Typography variant="subtitle1" fontWeight="200">
+            Show products
+          </Typography>
+        </Button>
+      </Menu>
     </Box>
   );
 }
